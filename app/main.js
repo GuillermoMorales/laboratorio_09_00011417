cb4ae72ecd9ace7e12e7ab23b9da638ada964dd4
GuillermoMorales <00011417@uca.edu.sv>
2018-10-26 09:07:59 -0600

    jaja

--- app/main.js
+++ app/main.js
@@ -13,15 +13,6 @@ mimeTypes = {
   "js": "text/javascript",
   "css": "text/css"
 };
-/*¿Cuál es la principal función del módulo HTTP?*/
-/*ermite realizar una petición de datos y recursos, como pueden ser documentos HTML. */
-
-/*¿Cuál es la principal función del módulo FileSystem?|*/
-/*permite trabajar con el sistema de archivos en su computadora.*/
-
-/*Que son los mime type*/
-/*son la manera standard de mandar contenido a través de la red. Los tipos MIME especifican tipos de datos, como por ejemplo texto, imagen, audio, etc. 
-que los archivos contienen. */
 
 
 http.createServer((req, res)=>{ 
@@ -147,37 +138,4 @@ http.createServer((req, res)=>{
 
     }).listen(8081);
 
-
-/*¿Qué contienen las variables "req" y "res" en la creación del servidor?*/
-/*la petición que se envia y el resultado*/
-
-/*¿La instrucción .listen(number) puede fallar? Justifique.*/
-/*Puede, si ese puerto esta siendo usado por otra aplicacion entonces fallaria*/
-
-/*¿Por qué es útil la función "collectRequestData(...)"?*/
-/*Pues esta función nos informa si el tipo de contenido es compatible o no, o lo contrario*/
-
-/*¿Para qué, además de conocer la dirección de la petición, es útil la variable "pathname"?*/
-/*Nos indica a que pagina es la que debe de mostrar segun la peticion*/
-
-/*¿Qué contine el parametro "data"? */
-/*Informacion*/
-
-/*¿Cuál es la diferencia entre brindar una respuesta HTML y brindar una CSS? */
-/*Que el tipo de texto cambia(dos tipos diferentes), entonces no se puede enviar ambos en una sola respuesta*/
-
-/*¿Qué contiene la variable "result"?*/
-/*Result viene a ser como el cambio que dio la respuesta, se colocara por ejemplo result(nuevo resultado).lastname, es decir el nuevo resultado en el lastname y asi para
-las demas variables se ira reemplazando*/
-
-/*¿Por qué con la variable "data" se debe aplicarse el metodo toString()? Justifique.*/
-/*Se debe de convertir a toString para poder manipularlo*/
-
-
-/*¿Se puede inciar el servidor (node main.js) en cualquier sitio del proyecto? Cualquier respuesta justifique. */
-/*No porque el archivo se encuentra en la carpeta de app*/
-
-/*Con sus palabras, ¿Por qué es importante aprender Node.js sin el uso de frameworks a pesar que estos facilitan el manejo de API's? */
-/*Es lo mismo como cuando aprendimos css, primero se aprende a "pata" para que luego usando frameworks sea mucho más fácil y sepamos usar mejor las
-nuevas herramientas que estos nos ofrecen*/
 
